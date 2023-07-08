f755255399500389077ab162af29e571a8b1c59b
VladimirKhil <vladimir.khil@gmail.com>
2023-07-08 14:05:19 +0200

    https://github.com/VladimirKhil/SIStatisticsService/issues/1 Support uniqueness for question text and theme name

---
+++ src/SIStatisticsService.Database/Migrations/AddUniqueness.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using SIStatisticsService.Database.Models.Questions;
+
+namespace SIStatisticsService.Database.Migrations;
+
+[Migration(202307080000, "Add uniqueness")]
+public sealed class AddUniqueness : Migration
+{
+    public override void Up()
+    {
+        Alter.Table(DbConstants.Questions)
+            .AlterColumn(nameof(QuestionModel.Text)).AsString().Unique().NotNullable();
+
+        Alter.Table(DbConstants.Themes)
+            .AlterColumn(nameof(ThemeModel.Name)).AsString().Unique().NotNullable();
+    }
+
+    public override void Down()
+    {
+        Alter.Table(DbConstants.Themes)
+            .AlterColumn(nameof(ThemeModel.Name)).AsString().NotNullable();
+
+        Alter.Table(DbConstants.Questions)
+    .       AlterColumn(nameof(QuestionModel.Text)).AsString().NotNullable();
+    }
+}
